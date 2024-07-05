05e5f2849f43c830b7f979b1e328a11454c37262
Bhavya-Thakral <abcbhavya3@gmail.com>
2024-07-05 11:23:08 +0530

    added pickvideo

--- src/screens/PickImage.js
+++ src/screens/PickImage.js
@@ -1,15 +1,20 @@
 import {launchImageLibrary} from 'react-native-image-picker';
 
 export const PickImage = (callback) => {
-    launchImageLibrary({mediaType:'photo'},(response)=>{
+    launchImageLibrary({mediaType:'mixed',quality:0.5,videoQuality:'medium'},(response)=>{
         if(response.didCancel){
-            console.log("User cancelled image picker");
+            console.log("User cancelled media picker");
         }
         else if(response.error){
-            console.log("Image picker error",response.error);
+            console.log("Media picker error",response.error);
         }
         else{
             const {uri , fileName , type} = response.assets[0];
+            if (type.startsWith('image/')) {
+                console.log("Picked an image");
+            } else if (type.startsWith('video/')) {
+                console.log("Picked a video");
+            }
             callback({uri,fileName,type});
         }
     });
